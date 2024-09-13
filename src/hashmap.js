fd95e70478c5f5b290b98f126728d80e0a587d27
Rahmat Alrafi Trikada <ralrafi12@gmail.com>
2024-09-13 11:00:19 +0700

    Add hash function

---
+++ src/hashmap.js
@@ -0,0 +1,12 @@
+function createHashMap() {
+    const hash = (key) => {
+        let hashCode = 0;
+
+        const primeNumber = 31;
+        for (let i = 0; i < key.length; i++) {
+            hashCode = primeNumber * hashCode + key.charCodeAt(i);
+        }
+
+        return hashCode;
+    }; 
+}
